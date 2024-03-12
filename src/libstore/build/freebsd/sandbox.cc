4e59bd39e82160f9f821a0a02a9be5b85ab33d80
Artemis Tosini <me@artem.ist>
2024-03-12 06:16:30 +0000

    freebsd/sandbox.cc: Fix formatting

--- src/libstore/build/freebsd/sandbox.cc
+++ src/libstore/build/freebsd/sandbox.cc
@@ -4,101 +4,102 @@
 #include "sandbox.hh"
 
 namespace nix {
-    void unmountAll(Path &path) {
-        int count;
-        struct statfs *mntbuf;
-        if ((count = getmntinfo(&mntbuf, MNT_WAIT)) < 0) {
-            throw SysError("Couldn't get mount info for chroot");
-        }
 
-        for (int i = 0; i < count; i++) {
-            Path mounted(mntbuf[i].f_mntonname);
-            if (hasPrefix(mounted, path)) {
-                if (unmount(mounted.c_str(), 0) < 0) {
-                    throw SysError("Failed to unmount path %1%", mounted);
-                }
+void unmountAll(Path &path) {
+    int count;
+    struct statfs *mntbuf;
+    if ((count = getmntinfo(&mntbuf, MNT_WAIT)) < 0) {
+        throw SysError("Couldn't get mount info for chroot");
+    }
+
+    for (int i = 0; i < count; i++) {
+        Path mounted(mntbuf[i].f_mntonname);
+        if (hasPrefix(mounted, path)) {
+            if (unmount(mounted.c_str(), 0) < 0) {
+                throw SysError("Failed to unmount path %1%", mounted);
             }
         }
     }
+}
 
-    void LocalDerivationGoal::chrootSetup(Path &chrootRootDir) {
-        unmountAll(chrootRootDir);
-        basicChrootSetup(chrootRootDir);
+void LocalDerivationGoal::chrootSetup(Path &chrootRootDir) {
+    unmountAll(chrootRootDir);
+    basicChrootSetup(chrootRootDir);
 
-        auto devpath = chrootRootDir + "/dev";
-        mkdir(devpath.c_str(), 0555);
-        mkdir((chrootRootDir + "/bin").c_str(), 0555);
-        char errmsg[255] = "";
+    auto devpath = chrootRootDir + "/dev";
+    mkdir(devpath.c_str(), 0555);
+    mkdir((chrootRootDir + "/bin").c_str(), 0555);
+    char errmsg[255] = "";
+    struct iovec iov[8] = {
+        { .iov_base = (void*)"fstype", .iov_len = sizeof("fstype") },
+        { .iov_base = (void*)"devfs", .iov_len = sizeof("devfs") },
+        { .iov_base = (void*)"fspath", .iov_len = sizeof("fspath") },
+        { .iov_base = (void*)devpath.c_str(), .iov_len = devpath.length() + 1 },
+        { .iov_base = (void*)"errmsg", .iov_len = sizeof("errmsg") },
+        { .iov_base = (void*)errmsg, .iov_len = sizeof(errmsg) },
+    };
+    if (nmount(iov, 6, 0) < 0) {
+        throw SysError("Failed to mount jail /dev: %1%", errmsg);
+    }
+    autoDelMounts.push_back(std::make_shared<AutoUnmount>(devpath));
+
+    /* Fixed-output derivations typically need to access the
+       network, so give them access to /etc/resolv.conf and so
+       on. */
+    if (!derivationType->isSandboxed()) {
+        // Only use nss functions to resolve hosts and
+        // services. Don’t use it for anything else that may
+        // be configured for this system. This limits the
+        // potential impurities introduced in fixed-outputs.
+        writeFile(chrootRootDir + "/etc/nsswitch.conf", "hosts: files dns\nservices: files\n");
+
+        /* N.B. it is realistic that these paths might not exist. It
+           happens when testing Nix building fixed-output derivations
+           within a pure derivation. */
+        for (auto & path : { "/etc/resolv.conf", "/etc/services", "/etc/hosts" })
+            if (pathExists(path))
+                pathsInChroot.try_emplace(path, path, true);
+
+        if (settings.caFile != "")
+            pathsInChroot.try_emplace("/etc/ssl/certs/ca-certificates.crt", settings.caFile, true);
+    }
+
+    for (auto & i : pathsInChroot) {
+        char errmsg[255];
+        errmsg[0] = 0;
+
+        if (i.second.source == "/proc") continue; // backwards compatibility
+        auto path = chrootRootDir + i.first;
+
+        struct stat stat_buf;
+        if (stat(i.second.source.c_str(), &stat_buf) < 0) {
+            throw SysError("stat");
+        }
+
+        // mount points must exist and be the right type
+        if (S_ISDIR(stat_buf.st_mode)) {
+            createDirs(path);
+        } else {
+            createDirs(dirOf(path));
+            writeFile(path, "");
+        }
+
         struct iovec iov[8] = {
             { .iov_base = (void*)"fstype", .iov_len = sizeof("fstype") },
-            { .iov_base = (void*)"devfs", .iov_len = sizeof("devfs") },
+            { .iov_base = (void*)"nullfs", .iov_len = sizeof("nullfs") },
             { .iov_base = (void*)"fspath", .iov_len = sizeof("fspath") },
-            { .iov_base = (void*)devpath.c_str(), .iov_len = devpath.length() + 1 },
+            { .iov_base = (void*)path.c_str(), .iov_len = path.length() + 1 },
+            { .iov_base = (void*)"target", .iov_len = sizeof("target") },
+            { .iov_base = (void*)i.second.source.c_str(), .iov_len = i.second.source.length() + 1 },
             { .iov_base = (void*)"errmsg", .iov_len = sizeof("errmsg") },
             { .iov_base = (void*)errmsg, .iov_len = sizeof(errmsg) },
         };
-        if (nmount(iov, 6, 0) < 0) {
-            throw SysError("Failed to mount jail /dev: %1%", errmsg);
+        if (nmount(iov, 8, 0) < 0) {
+            throw SysError("Failed to mount nullfs for %1% - %2%", path, errmsg);
         }
-        autoDelMounts.push_back(std::make_shared<AutoUnmount>(devpath));
-
-        /* Fixed-output derivations typically need to access the
-           network, so give them access to /etc/resolv.conf and so
-           on. */
-        if (!derivationType->isSandboxed()) {
-            // Only use nss functions to resolve hosts and
-            // services. Don’t use it for anything else that may
-            // be configured for this system. This limits the
-            // potential impurities introduced in fixed-outputs.
-            writeFile(chrootRootDir + "/etc/nsswitch.conf", "hosts: files dns\nservices: files\n");
-
-            /* N.B. it is realistic that these paths might not exist. It
-               happens when testing Nix building fixed-output derivations
-               within a pure derivation. */
-            for (auto & path : { "/etc/resolv.conf", "/etc/services", "/etc/hosts" })
-                if (pathExists(path))
-                    pathsInChroot.try_emplace(path, path, true);
-
-            if (settings.caFile != "")
-                pathsInChroot.try_emplace("/etc/ssl/certs/ca-certificates.crt", settings.caFile, true);
-        }
-
-        for (auto & i : pathsInChroot) {
-            char errmsg[255];
-            errmsg[0] = 0;
-
-            if (i.second.source == "/proc") continue; // backwards compatibility
-            auto path = chrootRootDir + i.first;
-
-            struct stat stat_buf;
-            if (stat(i.second.source.c_str(), &stat_buf) < 0) {
-                throw SysError("stat");
-            }
-
-            // mount points must exist and be the right type
-            if (S_ISDIR(stat_buf.st_mode)) {
-                createDirs(path);
-            } else {
-                createDirs(dirOf(path));
-                writeFile(path, "");
-            }
-
-            struct iovec iov[8] = {
-                { .iov_base = (void*)"fstype", .iov_len = sizeof("fstype") },
-                { .iov_base = (void*)"nullfs", .iov_len = sizeof("nullfs") },
-                { .iov_base = (void*)"fspath", .iov_len = sizeof("fspath") },
-                { .iov_base = (void*)path.c_str(), .iov_len = path.length() + 1 },
-                { .iov_base = (void*)"target", .iov_len = sizeof("target") },
-                { .iov_base = (void*)i.second.source.c_str(), .iov_len = i.second.source.length() + 1 },
-                { .iov_base = (void*)"errmsg", .iov_len = sizeof("errmsg") },
-                { .iov_base = (void*)errmsg, .iov_len = sizeof(errmsg) },
-            };
-            if (nmount(iov, 8, 0) < 0) {
-                throw SysError("Failed to mount nullfs for %1% - %2%", path, errmsg);
-            }
-            autoDelMounts.push_back(std::make_shared<AutoUnmount>(path));
-        }
+        autoDelMounts.push_back(std::make_shared<AutoUnmount>(path));
     }
+}
 
 void LocalDerivationGoal::createChild(const std::string &slaveName) {
     /* Now that we now the sandbox uid, we can write
@@ -156,5 +157,6 @@ void LocalDerivationGoal::createChild(const std::string &slaveName) {
         runChild();
     });
 }
+
 }
 #endif
